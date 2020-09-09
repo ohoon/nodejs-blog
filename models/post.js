cb528811700138777e0f391f90e0545a03cd5e39
ohoon <0hoon5661@gmail.com>
2020-09-09 18:40:06 +0900

    Add edit/delete post func

--- models/post.js
+++ models/post.js
@@ -4,7 +4,8 @@ module.exports = {
     create: async (body, user) => {
         const newPost = await db.query(
             `INSERT INTO posts(title, content, category_id, user_id, create_date) VALUES(?, ?, ?, ?, NOW())`
-            , [body.title, body.content, body.category_id, 1]);
+            , [body.title, body.content, body.category_id, 1]
+        );
 
         return newPost.insertId;
     },
@@ -18,9 +19,21 @@ module.exports = {
     read: async (postId) => {
         const posts = await db.query(
             `SELECT * FROM posts WHERE id=?`
-            , [postId]);
+            , [postId]
+        );
         
         return posts;
+    },
+    modify: async (postId, body) => {
+        await db.query(
+            `UPDATE posts SET title=?, content=?, category_id=?, create_date=Now() WHERE id=?`
+            , [body.title, body.content, body.category_id, postId]
+        );
+    },
+    destroy: async (postId) => {
+        await db.query(
+            `DELETE FROM posts WHERE id=?`
+            , [postId]
+        );
     }
-
 }
--- routes/posts.js
+++ routes/posts.js
@@ -19,10 +19,28 @@ router.get('/new', (req, res, next) => {
   res.render('posts/new', { });
 })
 
+/* Edit Post. */
+router.put('/:postId', async (req, res, next) => {
+  await post.modify(req.params.postId, req.body);
+  res.redirect(`/posts/${req.params.postId}`);
+})
+
+/* Edit Post Form. */
+router.get('/:postId/edit', async (req, res, next) => {
+  const posts = await post.read(req.params.postId);
+  res.render('posts/edit', { post: posts[0] });
+})
+
+/* Delete Post. */
+router.delete('/:postId', async (req, res, next) => {
+  await post.destroy(req.params.postId);
+  res.redirect(`/posts/`);
+})
+
 /* Show Post Content. */
 router.get('/:postId', async (req, res, next) => {
   const posts = await post.read(req.params.postId);
-  res.render('posts/read', { post: posts[0] });
+  res.render('posts/show', { post: posts[0] });
 })
 
 module.exports = router;
