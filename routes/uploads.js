387ade5a425fa2daa71cb1bcc80e756b856eeb84
ohoon <0hoon5661@gmail.com>
2020-09-26 19:47:28 +0900

    Add router for upload post request

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const fileDao = require('../models/File');
 const multer = require('multer');
-const uploadImage = multer({ dest: '/uploads/images' });
+const uploadImage = multer({ dest: 'public/uploads/images/' });
 
 /* Upload Images. */
 router.post('/images',
   uploadImage.single('upload'),
-  (req, res, next) => {
-    console.log(req.body);
-    console.log(req.file);
-    console.log(req.file.filename);
-    res.redirect('/');
+  async (req, res, next) => {
+    await fileDao.create(req.file);
+    res.json({ url: req.file.path.slice(6) });
   }
 );
 
