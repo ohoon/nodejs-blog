ae14556aa8ef4a570fc5ccb68cf0d42bb7ac8668
ohoon <0hoon5661@gmail.com>
2020-09-27 18:08:34 +0900

    Add dao func for assign file's post_id

--- models/File.js
+++ models/File.js
@@ -6,5 +6,27 @@ module.exports = {
             `INSERT INTO files(filename, path, size) VALUES(?, ?, ?)`
             , [file.filename, file.path.slice(6), file.size]
         );
+    },
+    assign: async (postId, images) => {
+        const size = images.length;
+        const where = [];
+
+        for (let i=0; i<size; i++) {
+            where.push("filename=?");
+        }
+
+        const queryText = where.join(" OR ");
+        const queryValue = [postId].concat(images);
+
+        await db.query(
+            `UPDATE files SET post_id=? WHERE post_id IS NULL AND ${queryText}`
+            , queryValue
+        );
+    },
+    unassign: async (postId) => {
+        await db.query(
+            `UPDATE files SET post_id=NULL WHERE post_id=?`
+            , [postId]
+        );
     }
 }
